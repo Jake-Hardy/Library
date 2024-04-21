a97af2a1426bb4a519976ea2fe35fb6b7b826fba
Jake-Hardy <jakehrdy@gmail.com>
2024-04-21 10:49:14 -0400

    feat: add update endpoint

---
+++ Library.Application/Features/BookFeatures/UpdateBook/UpdateBookHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Library.Application.Repositories;
+using Library.Domain.Entities;
+using MediatR;
+
+namespace Library.Application.Features.BookFeatures.UpdateBook;
+
+public sealed class UpdateBookHandler : IRequestHandler<UpdateBookRequest, UpdateBookResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IBookRepository _bookRepository;
+    private readonly IMapper _mapper;
+
+    public UpdateBookHandler(IUnitOfWork unitOfWork, IBookRepository bookRepository, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _bookRepository = bookRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<UpdateBookResponse> Handle(UpdateBookRequest request, CancellationToken cancellationToken)
+    {
+        var book = _mapper.Map<Book>(request);
+        _bookRepository.Update(book);
+        await _unitOfWork.Save(cancellationToken);
+
+        return _mapper.Map<UpdateBookResponse>(book);
+    }
+}
---
+++ Library.Application/Features/BookFeatures/UpdateBook/UpdateBookRequest.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Library.Application.Features.BookFeatures.UpdateBook;
+
+public sealed record UpdateBookRequest(Guid Id, string Title, string Author, string Genre) : IRequest<UpdateBookResponse>;
